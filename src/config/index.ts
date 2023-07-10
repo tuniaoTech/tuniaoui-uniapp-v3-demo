0262c5d252369660c16aa6c5a458e9045a61375a
jaylen <350683609@qq.com>
2023-07-10 11:24:56 +0800

    更新服务器url地址

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 // 上传图片地址
-export const UPLOAD_URL = 'https://vue3demo.tnkjapp.com/upload/image'
+export const UPLOAD_URL = 'https://vue3service.tuniaokj.com/upload/image'
 // export const UPLOAD_URL = 'http://127.0.0.1:8090/upload/image'
 
 // 获取githubData的地址
 export const GET_GITHUB_DATA_URL =
-  'https://vue3demo.tnkjapp.com/github_api/get_data'
+  'https://vue3service.tuniaokj.com/github_api/get_data'
 
 export * from './upload-headers'
